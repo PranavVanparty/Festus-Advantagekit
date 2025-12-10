1c8b78e6451c6ba356f910cbd3e8c81de0fb2a76
Jason Holt <you@example.com>
2025-12-10 17:30:59 -0500

    Neck Working on Trigger

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -32,12 +32,13 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.Constants.OIConstants;
 import frc.robot.commands.DriveCommands;
+import frc.robot.commands.MoveNeck;
 import frc.robot.subsystems.Neck;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.drive.DriveConstants;
@@ -199,11 +200,11 @@ public class RobotContainer {
                             Degrees.of(-60)))));
         }
 
-        m_Neck.setDefaultCommand(new RunCommand(() -> m_Neck.move(-gunner.getLeftY()), m_Neck));
+        // m_Neck.setDefaultCommand(new RunCommand(() -> m_Neck.move(-gunner.getLeftY()), m_Neck));
         // what simon added starts here
         // Change to whileTrue after re-maping for climer
-        //         new JoystickButton(m_gunnerController, Button.kA.value)
-        //                 .onTrue(new ShootAMP(m_robotShooter, m_robotIntake, m_Neck));
+        new Trigger(() -> m_gunnerController.getLeftY() != 0)
+                .whileTrue(new MoveNeck(m_Neck, () -> -m_gunnerController.getLeftY()));
 
         //         new JoystickButton(m_gunnerController, Button.kX.value)
         //                 // .onTrue(new NeckRaiseAndShoot(m_Neck, 0.0887+0.004, m_robotShooter, m_robotIntake));
